a452fe43a0bbbb7208730db7d90793211e59d01e
heedii <127703762+heedii@users.noreply.github.com>
2023-05-30 19:26:41 -0700

    added omni mode to sprint

--- src/main/java/club/l4j/currymod/feature/impl/hackimpl/movement/Sprint.java
+++ src/main/java/club/l4j/currymod/feature/impl/hackimpl/movement/Sprint.java
@@ -9,26 +9,37 @@ import demo.knight.demobus.event.DemoListen;
 @Hack.Construct(name = "Sprint", description = "sprint", category = Hack.Category.MOVEMENT)
 public class Sprint extends Hack {
 
-    OptionMode mode = new OptionMode("Mode", "Rage","Rage","Legit");
+    OptionMode mode = new OptionMode("Mode", "Rage", "Rage", "Omni");
 
-    public Sprint(){
+    public Sprint() {
         addOptions(mode);
     }
 
     @DemoListen
     public void onTick(TickEvent e) {
-        if(nullCheck()){return;}
-        if(mode.isMode("Rage")) {
+        if (nullCheck()) {
+            return;
+        }
+        if (mode.isMode("Rage")) {
             mc.player.setSprinting(true);
-        }else{
-            if(MovementUtils.isMoving() && !mc.player.isSneaking()){
+        } else {
+            if (MovementUtils.isMoving() && !mc.player.isSneaking()) {
                 mc.player.setSprinting(true);
             }
+            if (mode.isMode("Omni")) {
+                if (mc.options.forwardKey.isPressed()) {
+                    mc.player.setSprinting(true);
+                }
+                if (mc.options.leftKey.isPressed()) {
+                    mc.player.setSprinting(true);
+                }
+                if (mc.options.rightKey.isPressed()) {
+                    mc.player.setSprinting(true);
+                }
+                if (mc.options.backKey.isPressed()) {
+                    mc.player.setSprinting(true);
+                }
+            }
         }
-    }
-
-    @Override
-    public String getContent() {
-        return "["+mode.getMode()+"]";
     }
 }
