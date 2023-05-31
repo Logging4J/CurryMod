912e9c6e6df19f9d823920be69fc9ffde27b90d3
heedii <127703762+heedii@users.noreply.github.com>
2023-05-31 07:42:36 -0700

    created confetti [wip]

--- src/main/java/club/l4j/currymod/feature/impl/hackimpl/combat/RayTraceBypass.java
+++ src/main/java/club/l4j/currymod/feature/impl/hackimpl/combat/RayTraceBypass.java
@@ -4,5 +4,4 @@ import club.l4j.currymod.feature.core.Hack;
 
 @Hack.Construct(name = "[WIP]RaytraceBypass", description = "bypasses raytrace", category = Hack.Category.COMBAT)
 public class RayTraceBypass extends Hack {
-    
 }
