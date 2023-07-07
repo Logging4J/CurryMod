5708b6984790cb720d03375db87313b84b0ffbd3
Logging4J <80828850+Logging4J@users.noreply.github.com>
2023-07-07 10:59:11 -0400

    WIP

--- src/main/java/club/l4j/currymod/feature/impl/hackimpl/visual/GuiBackground.java
+++ src/main/java/club/l4j/currymod/feature/impl/hackimpl/visual/GuiBackground.java
@@ -3,7 +3,7 @@ package club.l4j.currymod.feature.impl.hackimpl.visual;
 import club.l4j.currymod.feature.core.Hack;
 import club.l4j.currymod.feature.options.impl.OptionMode;
 
-@Hack.Construct(name = "Gui Background",description = "Changes the background of the gui", category = Hack.Category.VISUAL)
+@Hack.Construct(name = "GuiBackground(WIP)",description = "Changes the background of the gui", category = Hack.Category.VISUAL)
 public class GuiBackground extends Hack {
 
     public OptionMode mode = new OptionMode("Mode", "Remove", "Remove", "Blur");
