d0dd64da9587a69c88cdd1c2bd3643a9dbdb4b3a
Logging4J <80828850+Logging4J@users.noreply.github.com>
2023-09-02 20:36:24 -0800

    velocity

--- src/main/java/wtf/l4j/impl/modules/combat/PlayerModifier.java
+++ src/main/java/wtf/l4j/impl/modules/combat/PlayerModifier.java
@@ -5,6 +5,7 @@ import de.florianmichael.dietrichevents2.DietrichEvents2;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 
+import net.minecraft.network.packet.s2c.query.QueryPongS2CPacket;
 import wtf.l4j.api.module.Category;
 import wtf.l4j.api.module.Module;
 import wtf.l4j.api.module.ModuleInfo;
@@ -12,17 +13,19 @@ import wtf.l4j.api.module.option.options.OptionBoolean;
 import wtf.l4j.api.event.GameTickListener;
 import wtf.l4j.api.event.PacketListener;
 import wtf.l4j.api.event.Type;
+import wtf.l4j.api.module.option.options.OptionMode;
 import wtf.l4j.api.utils.MovementUtils;
 
 @ModuleInfo(name = "PlayerModifier", desc = "Modify player", category = Category.COMBAT)
 public class PlayerModifier extends Module implements GameTickListener, PacketListener {
 
+    //Hit Velocity for 2b WIP
+    public OptionMode mode = new OptionMode("Mode", "Cancel", "Cancel","2b2t");
     public static OptionBoolean push = new OptionBoolean("NoPush", true);
-    public static OptionBoolean velo = new OptionBoolean("NoVelocity", true);
     public static OptionBoolean fastStop = new OptionBoolean("FastStop", true);
 
     public PlayerModifier(){
-        addOptions(push, velo, fastStop);
+        addOptions(push, mode, fastStop);
     }
 
     @Override
@@ -42,15 +45,19 @@ public class PlayerModifier extends Module implements GameTickListener, PacketLi
     @Override
     public void onGameTick() {
         if(!MovementUtils.isMoving() && fastStop.isEnabled()){
-            mc.player.setVelocity(0,mc.player.getVelocity().y,0);
+            mc.player.setVelocity(0, mc.player.getVelocity().y, 0);
         }
     }
 
     @Override
     public void onPacket(PacketEvent packetEvent) {
         if(packetEvent.getType() == Type.INCOMING){
-            if(velo.isEnabled()) {
-                if (packetEvent.getPacket() instanceof ExplosionS2CPacket || (packetEvent.getPacket() instanceof EntityVelocityUpdateS2CPacket p && p.getId() == mc.player.getId())) {
+            if (packetEvent.getPacket() instanceof ExplosionS2CPacket) {
+                if(mode.isMode("Cancel") || mode.isMode("2b2t")) {
+                    packetEvent.setCancelled(true);
+                }
+            } else if (packetEvent.getPacket() instanceof EntityVelocityUpdateS2CPacket) {
+                if(mode.isMode("Cancel")){
                     packetEvent.setCancelled(true);
                 }
             }
