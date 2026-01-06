0b5bbdb03e3f76c9df4426461071edb03e0a9444
logging4j <logging4j@gmail.com>
2026-01-05 23:46:13 -0500

    fix build failures in workflow

--- src/main/java/dev/logging4j/currymod/GitInfo.java
+++ src/main/java/dev/logging4j/currymod/GitInfo.java
@@ -7,12 +7,12 @@ public final class GitInfo {
 	public static final String MAVEN_GROUP = "dev.logging4j";
 	public static final String MAVEN_NAME = "CurryMod";
 	public static final String VERSION = "1.0-SNAPSHOT";
-	public static final int GIT_REVISION = 185;
-	public static final String GIT_SHA = "bee4d4e720068de9bee89e3d4b4bdfaaf619fb78";
-	public static final String GIT_DATE = "2026-01-03T18:21:12Z";
+	public static final int GIT_REVISION = 187;
+	public static final String GIT_SHA = "365c683be9fd2a748bfa86958ab5dabf329a2e7c";
+	public static final String GIT_DATE = "2026-01-05T23:39:30Z";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2026-01-05T23:28:42Z";
-	public static final long BUILD_UNIX_TIME = 1767673722608L;
+	public static final String BUILD_DATE = "2026-01-05T23:45:09Z";
+	public static final long BUILD_UNIX_TIME = 1767674709032L;
 	public static final int DIRTY = 1;
 
 	private GitInfo(){}
