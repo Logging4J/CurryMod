6d74d69c9f4ecae39118eb7495c83f29e8893db1
logging4j <logging4j@gmail.com>
2026-01-16 00:44:01 -0500

    XCarry

--- src/main/java/dev/logging4j/currymod/GitInfo.java
+++ src/main/java/dev/logging4j/currymod/GitInfo.java
@@ -7,12 +7,12 @@ public final class GitInfo {
 	public static final String MAVEN_GROUP = "dev.logging4j";
 	public static final String MAVEN_NAME = "CurryMod";
 	public static final String VERSION = "1.0-SNAPSHOT";
-	public static final int GIT_REVISION = 190;
-	public static final String GIT_SHA = "45e832d0085579ee16abd01c870f1e4a8a623d02";
-	public static final String GIT_DATE = "2026-01-06T08:45:50Z";
+	public static final int GIT_REVISION = 191;
+	public static final String GIT_SHA = "35206b4697d8d528ae7effe393bcdea1ae7e3537";
+	public static final String GIT_DATE = "2026-01-07T19:40:43Z";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2026-01-06T20:54:46Z";
-	public static final long BUILD_UNIX_TIME = 1767750886252L;
+	public static final String BUILD_DATE = "2026-01-16T00:35:24Z";
+	public static final long BUILD_UNIX_TIME = 1768541724985L;
 	public static final int DIRTY = 1;
 
 	private GitInfo(){}
